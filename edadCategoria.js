7d5324172a78091936b366ca095bc8d2615aa169
Ermunizp <ermuniz@espe.edu.ec>
2025-05-18 18:30:27 -0500

    Creación de Index y programa Js clasificador de edad

---
+++ edadCategoria.js
@@ -0,0 +1,24 @@
+// Solicitar edad al usuario
+let edad = prompt("Ingrese su edad:");
+
+// Convertir la entrada a número
+edad = Number(edad);
+
+// Clasificación utilizando IF anidados
+if (edad < 0) {
+  console.log("Edad no válida");
+} else {
+  if (edad <= 12) {
+    console.log("Niño");
+  } else {
+    if (edad <= 17) {
+      console.log("Adolescente");
+    } else {
+      if (edad <= 59) {
+        console.log("Adulto");
+      } else {
+        console.log("Adulto mayor");
+      }
+    }
+  }
+}
