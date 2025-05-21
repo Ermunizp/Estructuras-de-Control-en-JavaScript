395cc9a593efa156a2e82255d31bbccddadfa732
Ermunizp <ermuniz@espe.edu.ec>
2025-05-20 21:43:07 -0500

    Corrección de variable a multiplicar

--- tablaMultiplicar.js
+++ tablaMultiplicar.js
@@ -1,16 +1,16 @@
 // Obtener el número ingresado y convertirlo a número
-let numero = Number(window.numeroIngresado);
+let numeroMultiplicar = Number(window.numeroIngresado);
 
-// Validación usando if anidados
+// Validación de número ingresado (isNaN) y uso de if anidados
 if (isNaN(numero)) {
   console.log("Entrada no válida. Por favor, ingrese un número.");
 } else {
   if (numero <= 0) {
     console.log("Ingrese un número mayor que cero.");
   } else {
-    console.log(`Tabla de multiplicar del ${numero}:`);
+    console.log(`Tabla de multiplicar del ${numeroMultiplicar}:`);
     for (let i = 1; i <= 12; i++) {
-      console.log(`${numero} x ${i} = ${numero * i}`);
+      console.log(`${numeroMultiplicar} x ${i} = ${numeroMultiplicar * i}`);
     }
   }
 }
